add98b1903384ea54ce2212d871c4e5024597be3
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-11-21 15:58:00 +0900

    [update] エントリーの追加ができるようになった

--- main/api_handler.py
+++ main/api_handler.py
@@ -1,5 +1,5 @@
-import flask 
-from main import app
+import flask
+from main import app,db
 from main.models import Entry # 追加
 from main.toJson import *
 
@@ -7,10 +7,25 @@ from main.toJson import *
 def helloworld():
     return 'Hello, World!'
 
-@app.route('/entries')
+@app.route('/',methods=['POST'])
+def oumugaeshi():
+    return flask.request.get_data()
+
+@app.route('/entries/')
 def show_entries():
     entries = Entry.query.all() # 追加
-    print(entries)
-    # todo: tojson
-    print(convert_entries_to_json(entries))
     return convert_entries_to_json(entries)
+
+@app.route('/entries/add',methods=['POST'])
+#TODO: ログイン
+#TODO: エラーハンドリング
+def add_entry():
+    new_entry_json=flask.request.json
+    entry = Entry(
+        title = new_entry_json['title'],
+        text = new_entry_json['text']
+    )
+    db.session.add(entry)
+    db.session.commit()
+    print("data pushed",new_entry_json)
+    return "data pushed"
