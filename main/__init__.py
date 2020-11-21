bc8e91c83cdef4053bb19a60c2f3c01a93041f96
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-11-21 17:25:54 +0900

    [add] dotenvを追加。

--- main/__init__.py
+++ main/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 app.config.from_object('main.config') 
+
 app.config["JSON_AS_ASCII"] = False
 
 db = SQLAlchemy(app) 
