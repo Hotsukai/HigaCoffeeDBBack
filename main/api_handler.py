5715284d1c1649a6a7c8fdb606ae3ed529da3775
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-11-30 14:57:01 +0900

    update ログインができるようになった

--- main/api_handler.py
+++ main/api_handler.py
@@ -1,8 +1,9 @@
+from logging import log
 import flask
 from main import app, db, bcrypt
 from main.models import Entry  ,User
 from main.utils import *
-
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 @app.route('/')
 def helloworld():
@@ -34,7 +35,7 @@ def add_entry():
     print("data pushed", new_entry_json)
     return flask.jsonify({"message": "data pushed"})
 
-
+#user Create
 @app.route('/auth/create_user/', methods=['POST'])
 def create_user():
     form_data = flask.request.json
@@ -54,7 +55,9 @@ def create_user():
     db.session.add(user)
     db.session.commit()
     return flask.jsonify({"message": "ユーザー("+username+")を作成しました。"})
-
+#user index TODO:
+#user show
+#user update 
 @app.route('/auth/login/', methods=['POST'])
 def login():
     form_data = flask.request.json
@@ -65,12 +68,13 @@ def login():
         return flask.jsonify({"message": "ユーザー名は必須です"}), 400
     if not password:
         return flask.jsonify({"message": "パスワードは必須です"}), 400
-    users = User.query.filter_by(name = username).all()
-    if len(users)!=0:
-        return flask.jsonify({"message": "ユーザー名が利用されています。"}), 400
-
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    user = User(name=username, encrypted_password=hashed_password)
-    db.session.add(user)
-    db.session.commit()
-    return flask.jsonify({"message": "ユーザー("+username+")を作成しました。"})
+    user = User.query.filter_by(name = username).first()
+    if user and bcrypt.check_password_hash(user.encrypted_password, password):
+        #ログイン成功
+        login_user(user)
+        print("ログイン成功")
+        return flask.jsonify({"message": "ユーザー("+username+")のログインに成功しました。"})
+    else:
+        print("ログイン失敗")
+        return flask.jsonify({"message": "ユーザー("+username+")のログインに失敗しました。"})
+        
