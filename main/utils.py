f62ae02277ac54a173d914e9fd1f34cd55b41f18
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-12-08 23:59:52 +0900

    [fix] リファクタリング

--- main/utils.py
+++ main/utils.py
@@ -5,20 +5,7 @@ from flask.json import jsonify
 def convert_coffees_to_json(coffees):
     json = []
     for coffee in coffees:
-        json.append({
-            "powderAmount": coffee.powder_amount,
-            "id": coffee.id,
-            "extractionTime": coffee.extraction_time,
-            "extractionMethod_id": coffee.extraction_method_id,
-            "meshId": coffee.mesh_id,
-            "waterAmount": coffee.water_amount,
-            "waterTemperature": coffee.water_temperature,
-            "beanId": coffee.bean_id,
-            "bean": BEAN[coffee.bean_id],
-            "memo": coffee.memo,
-            "dripperId": coffee.dripper.id,
-            "createdAt": coffee.created_at
-        })
+        json.append(convert_coffee_to_json(coffee))
     return json
 
 
@@ -27,8 +14,9 @@ def convert_coffee_to_json(coffee):
         "powderAmount": coffee.powder_amount,
         "id": coffee.id,
         "extractionTime": coffee.extraction_time,
-        "extractionMethod_id": coffee.extraction_method_id,
+        "extractionMethodId": coffee.extraction_method_id,
         "meshId": coffee.mesh_id,
+        # TODO: mesh,method,dripperのname
         "waterAmount": coffee.water_amount,
         "waterTemperature": coffee.water_temperature,
         "beanId": coffee.bean_id,
@@ -65,16 +53,5 @@ def convert_review_to_json(review):
 def convert_reviews_to_json(reviews):
     json = []
     for review in reviews:
-        json.append({
-            "id": review.id,
-            "bitterness": review.bitterness,
-            "wantRepeat": review.want_repeat,
-            "situation": review.situation,
-            "strongness": review.strongness,
-            "feeling": review.feeling,
-            "createdAt": review.created_at,
-            "updatedAt": review.updated_at,
-            "coffee": convert_coffee_to_json(Coffee.query.get(review.coffee_id))
-
-        })
+        json.append(convert_review_to_json(review))
     return json
