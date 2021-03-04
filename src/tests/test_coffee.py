fc062d83f54c2c2c9d1b4be7e36308a818561043
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-03-04 14:28:51 +0900

    add コーヒーテストのリファクタリング

--- src/tests/test_coffee.py
+++ src/tests/test_coffee.py
@@ -1,26 +1,11 @@
-from datetime import timezone
 import json
-from operator import add
 
 from flask_jwt_extended import create_access_token
 
-from src.models.models import BEANS, Coffee
+from src.models.models import BEANS, Coffee, EXTRACTION_METHOD
 from src.database import db
 from .base import BaseTestCase
 from .test_utils import add_sample_users, format_datetime_to_json_str
-
-valid_coffee_param = {
-    "beanId": 2,
-    "drinkerIds": [1],
-    "dripperId": 1,
-    "extractionMethodId": 1,
-    "extractionTime": 8,
-    "memo": "",
-    "meshId": None,
-    "powderAmount": 8.7,
-    "waterAmount": 98,
-    "waterTemperature": 3,
-}
 
 
 class TestCoffee(BaseTestCase):
@@ -33,7 +18,7 @@ class TestCoffee(BaseTestCase):
         self.assert401(response)
 
     def test_コーヒーが登録できる(self):
-        user1, _ = add_sample_users()
+        user1, user2 = add_sample_users()
         token: str = create_access_token(user1)
         postParams = {
             "beanId": 1,
@@ -52,17 +37,67 @@ class TestCoffee(BaseTestCase):
                                  headers={"Authorization": f"Bearer {token}"},
                                  content_type='application/json')
         self.assert_status(response, 200)
+        coffee: Coffee = Coffee.query.get(1)
         data = json.loads(response.get_data())
-        _bean = list(
-            filter(lambda bean: bean.id == postParams["beanId"], BEANS))[0]
-        assert data['result'] is True
-        assert data['data']["id"] == 1
-        assert data['data']["bean"]["fullName"] == _bean.name
-        assert data['data']["bean"]["detail"] == _bean.detail
-        assert data['data']["bean"]["roast"]["id"] == _bean.roast.value
-        assert data['data']["bean"]["roast"]["name"] == _bean.roast.name
-        assert data['data']["bean"]["origin"]["id"] == _bean.origin.value
-        assert data['data']["bean"]["origin"]["name"] == _bean.origin.name
+        assert data == {
+            'data': {
+                "bean":
+                BEANS[0].to_json(),
+                'createdAt':
+                format_datetime_to_json_str(coffee.created_at),
+                'drinkers': [{
+                    'created_at':
+                    format_datetime_to_json_str(user1.created_at),
+                    'id':
+                    user1.id,
+                    'name':
+                    user1.name,
+                    'profile':
+                    user1.profile,
+                    'updated_at':
+                    format_datetime_to_json_str(user1.updated_at)
+                }, {
+                    'created_at':
+                    format_datetime_to_json_str(user2.created_at),
+                    'id':
+                    user2.id,
+                    'name':
+                    user2.name,
+                    'profile':
+                    user2.profile,
+                    'updated_at':
+                    format_datetime_to_json_str(user2.updated_at)
+                }],
+                'dripper': {
+                    'created_at':
+                    format_datetime_to_json_str(user1.created_at),
+                    'id': user1.id,
+                    'name': user1.name,
+                    'profile': user1.profile,
+                    'updated_at': format_datetime_to_json_str(user1.updated_at)
+                },
+                'extractionMethod':
+                EXTRACTION_METHOD[1],
+                'extractionTime':
+                8,
+                'id':
+                1,
+                'memo':
+                '',
+                'mesh':
+                None,
+                'powderAmount':
+                8.7,
+                'reviewId': [],
+                'reviews': [],
+                'waterAmount':
+                98,
+                'waterTemperature':
+                3
+            },
+            'message': 'コーヒーを作成しました。',
+            'result': True
+        }
 
     def test_ログイン無しでコーヒーを1件取得できる(self):
         user1, user2 = add_sample_users()
